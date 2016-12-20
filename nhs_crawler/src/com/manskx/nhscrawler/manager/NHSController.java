889352ecf4c5658ec5ebfb042a1200cce1136300
Mansy <ahmedm@mgc.mentorg.com>
2016-12-20 18:31:19 +0200

    adding background thread

--- nhs_crawler/src/com/manskx/nhscrawler/manager/NHSController.java
+++ nhs_crawler/src/com/manskx/nhscrawler/manager/NHSController.java
@@ -5,8 +5,11 @@ import edu.uci.ics.crawler4j.crawler.CrawlController;
 import edu.uci.ics.crawler4j.fetcher.PageFetcher;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
 public class NHSController {
 	private static NHSController instance;
+	public static boolean CrawlingStarted = false;
+	public static boolean CrawlingFinished = false;
 
 	private NHSController() {
 	}
@@ -18,86 +21,119 @@ public class NHSController {
 		return instance;
 	}
 
-	
-	public void crawler() throws Exception{
-		 /*
-	     * crawlStorageFolder is a folder where intermediate crawl data is
-	     * stored.
-	     */
-	        String crawlStorageFolder = "/data/";
+	public String getCrawlingStatus() {
+		if (CrawlingStarted)
+			return "Crawling is running now !";
+		if (CrawlingFinished)
+			return "Crawling is finished !";
+		return "Crawling is not started yet";
 
-	    /*
-	     * numberOfCrawlers shows the number of concurrent threads that should
-	     * be initiated for crawling.
-	     */
-	        int numberOfCrawlers = 5;
+	}
 
-	        CrawlConfig config = new CrawlConfig();
+	public String startCrawlingInBackground() throws Exception {
 
-	        config.setCrawlStorageFolder(crawlStorageFolder);
+		if (CrawlingStarted)
+			return "Crawling is running now, Please try again after finishing crawling";
+		Runnable r = new Runnable() {
+			public void run() {
+				try {
+					crawler();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		};
 
-	    /*
-	     * Be polite: Make sure that we don't send more than 1 request per
-	     * second (1000 milliseconds between requests).
-	     */
-	        config.setPolitenessDelay(1000);
+		new Thread(r).start();
 
-	    /*
-	     * You can set the maximum crawl depth here. The default value is -1 for
-	     * unlimited depth
-	     */
-	        config.setMaxDepthOfCrawling(10);
+		if (CrawlingStarted)
+			return "Crawling is running now !";
+		return "An error oqquired please try contact admin";
+	}
 
-	    /*
-	     * You can set the maximum number of pages to crawl. The default value
-	     * is -1 for unlimited number of pages
-	     */
-	        config.setMaxPagesToFetch(1000);
+	public void crawler() throws Exception {
+		CrawlingStarted = true;
+		/*
+		 * crawlStorageFolder is a folder where intermediate crawl data is
+		 * stored.
+		 */
+		String crawlStorageFolder = "/data/";
 
-	        /**
-	         * Do you want crawler4j to crawl also binary data ?
-	         * example: the contents of pdf, or the metadata of images etc
-	         */
-	        config.setIncludeBinaryContentInCrawling(false);
+		/*
+		 * numberOfCrawlers shows the number of concurrent threads that should
+		 * be initiated for crawling.
+		 */
+		int numberOfCrawlers = 5;
 
-	    /*
-	     * Do you need to set a proxy? If so, you can use:
-	     * config.setProxyHost("proxyserver.example.com");
-	     * config.setProxyPort(8080);
-	     *
-	     * If your proxy also needs authentication:
-	     * config.setProxyUsername(username); config.getProxyPassword(password);
-	     */
+		CrawlConfig config = new CrawlConfig();
 
-	    /*
-	     * This config parameter can be used to set your crawl to be resumable
-	     * (meaning that you can resume the crawl from a previously
-	     * interrupted/crashed crawl). Note: if you enable resuming feature and
-	     * want to start a fresh crawl, you need to delete the contents of
-	     * rootFolder manually.
-	     */
-	        config.setResumableCrawling(false);
+		config.setCrawlStorageFolder(crawlStorageFolder);
 
-	    /*
-	     * Instantiate the controller for this crawl.
-	     */
-	        PageFetcher pageFetcher = new PageFetcher(config);
-	        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
-	        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
-	        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+		/*
+		 * Be polite: Make sure that we don't send more than 1 request per
+		 * second (1000 milliseconds between requests).
+		 */
+		config.setPolitenessDelay(1000);
 
-	    /*
-	     * For each crawl, you need to add some seed urls. These are the first
-	     * URLs that are fetched and then the crawler starts following links
-	     * which are found in these pages
-	     */
-	        controller.addSeed("http://www.nhs.uk/conditions/Pages/hub.aspx");
+		/*
+		 * You can set the maximum crawl depth here. The default value is -1 for
+		 * unlimited depth
+		 */
+		config.setMaxDepthOfCrawling(10);
 
+		/*
+		 * You can set the maximum number of pages to crawl. The default value
+		 * is -1 for unlimited number of pages
+		 */
+		config.setMaxPagesToFetch(1000);
 
-	    /*
-	     * Start the crawl. This is a blocking operation, meaning that your code
-	     * will reach the line after this only when crawling is finished.
-	     */
-	        controller.start(NHSCrawler.class, numberOfCrawlers);
+		/**
+		 * Do you want crawler4j to crawl also binary data ? example: the
+		 * contents of pdf, or the metadata of images etc
+		 */
+		config.setIncludeBinaryContentInCrawling(false);
+
+		/*
+		 * Do you need to set a proxy? If so, you can use:
+		 * config.setProxyHost("proxyserver.example.com");
+		 * config.setProxyPort(8080);
+		 *
+		 * If your proxy also needs authentication:
+		 * config.setProxyUsername(username); config.getProxyPassword(password);
+		 */
+
+		/*
+		 * This config parameter can be used to set your crawl to be resumable
+		 * (meaning that you can resume the crawl from a previously
+		 * interrupted/crashed crawl). Note: if you enable resuming feature and
+		 * want to start a fresh crawl, you need to delete the contents of
+		 * rootFolder manually.
+		 */
+		config.setResumableCrawling(false);
+
+		/*
+		 * Instantiate the controller for this crawl.
+		 */
+		PageFetcher pageFetcher = new PageFetcher(config);
+		RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+		RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+		CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+		/*
+		 * For each crawl, you need to add some seed urls. These are the first
+		 * URLs that are fetched and then the crawler starts following links
+		 * which are found in these pages
+		 */
+		controller.addSeed("http://www.nhs.uk/conditions/Pages/hub.aspx");
+
+		/*
+		 * Start the crawl. This is a blocking operation, meaning that your code
+		 * will reach the line after this only when crawling is finished.
+		 */
+		controller.start(NHSCrawler.class, numberOfCrawlers);
+		CrawlingFinished = true;
+		CrawlingStarted = false;
+
 	}
 }
